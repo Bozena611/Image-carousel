0a4ab4b2c81e2d029bc43edaea5ae394bcd167b5
Bozena611 <bozena.vuckovic@gmail.com>
2021-02-22 20:57:20 +0100

    added description

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,6 @@
 // Write your code here
 let img = document.getElementById("images");
+let description = document.getElementById("text");
 let back = document.getElementById("back");
 let autoBack = document.getElementById("auto-back");
 let forward = document.getElementById("forward");
@@ -8,14 +9,21 @@ let stopAuto = document.getElementById("stop");
 
 let images = [
 	{
+		description: "How you feel is how you vibrate",
 		link: "./IMG/Emotional-Vibrational-Scale.png"
 	},
 	{
+		description: "The better your feelings the higher your energy",
+		link: "./IMG/Emotional-Vibrational-Chart.png"
+	},
+	{
+		description: "Chakras and Energy connection",
 		link: "./IMG/Chakras.jpg"
 	},
 	{
-		link: "./IMG/Emotional-Vibrational-Chart.png"
-	},
+		description: "Balance Your Chakras With Solfeggio Frequencies",
+		link: "./IMG/SolfeggioandChakras.jpg"
+	}
 ]
 
 let index = 0;
@@ -24,6 +32,7 @@ function goForward(array) {
 	index = index+1;
 	index = index % array.length
 	console.log(array[index])
+	description.innerText = array[index].description;
 	img.src=array[index].link;
 }
 
@@ -52,12 +61,12 @@ back.addEventListener("click", ()=> {
 
 autoForward.addEventListener("click", ()=> {
 	interval = setInterval(function() {
-		goForward(images)}, 1000);
+		goForward(images)}, 1200);
 })
 
 autoBack.addEventListener("click", ()=> {
 	interval = setInterval(function() {
-		goBackward(images)}, 1000);
+		goBackward(images)}, 1200);
 })
 
 stopAuto.addEventListener("click", ()=> {
