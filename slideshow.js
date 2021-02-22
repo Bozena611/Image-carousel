0907f2ec2ecbf67e821b406e9cfe386b6af62d9f
Bozena611 <bozena.vuckovic@gmail.com>
2021-02-22 19:02:18 +0100

    back+auto+stop

--- slideshow.js
+++ slideshow.js
@@ -3,8 +3,8 @@ let img = document.getElementById("images");
 let back = document.getElementById("back");
 let autoBack = document.getElementById("auto-back");
 let forward = document.getElementById("forward");
-let autForward = document.getElementById("auto-forward");
-let stop = document.getElementById("stop");
+let autoForward = document.getElementById("auto-forward");
+let stopAuto = document.getElementById("stop");
 
 let images = [
 	{
@@ -20,13 +20,46 @@ let images = [
 
 let index = 0;
 
-function moveImages(array) {
+function goForward(array) {
 	index = index+1;
 	index = index % array.length
 	console.log(array[index])
 	img.src=array[index].link;
 }
 
+function goBackward(array) {
+	index--
+	if (index<0) {
+		index = array.length-1
+	}
+	console.log(array[index])
+	img.src=array[index].link
+}
+
+let interval;
+
+function stop(){
+	clearInterval(interval);
+}
+
 forward.addEventListener("click", () => {
-	moveImages(images);
+	goForward(images);
+})
+
+back.addEventListener("click", ()=> {
+	goBackward(images);
+})
+
+autoForward.addEventListener("click", ()=> {
+	interval = setInterval(function() {
+		goForward(images)}, 1000);
+})
+
+autoBack.addEventListener("click", ()=> {
+	interval = setInterval(function() {
+		goBackward(images)}, 1000);
+})
+
+stopAuto.addEventListener("click", ()=> {
+	stop();
 })
